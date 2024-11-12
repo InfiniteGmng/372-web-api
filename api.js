c30fd162beebe24f26d4928697cf81519a738663
Noah Lanctot <135896269+InfiniteGmng@users.noreply.github.com>
2024-11-12 14:50:57 -0800

    web api project

---
+++ api.js
@@ -0,0 +1,17 @@
+import express from "express";
+// import productRouter from "./routers/productsRouter.js";
+import jokesRouter from "./routers/jokesRouter.js";
+
+const app = express();
+
+//serve up static files from the /public directory
+app.use(express.static("./public"));
+
+//configure your web server to parse JSON in a request body
+app.use(express.json());
+
+//mount the router!
+// app.use("/api/v1/products", productRouter);
+app.use("/api/v1/jokes", jokesRouter);
+
+app.listen(4242, () => console.log(`Server started on port 4242`));
